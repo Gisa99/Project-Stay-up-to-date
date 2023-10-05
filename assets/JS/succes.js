3c717b224dbea8e0e267edaac134824b96ab34c5
Gisele <rgisele644@gmail.com>
2023-10-05 18:44:39 -0300

    Navegação entre páginas finalizada

--- assets/JS/succes.js
+++ assets/JS/succes.js
@@ -11,18 +11,26 @@ function funcaoPaginaCarregada() {
     const criaCard = document.createElement("div"); // Correção: remova o ponto do ".card"
     criaCard.className = "card";
     criaCard.innerHTML = `
-    <img src="../images/icon-success.svg" alt="success icon">
-    <h1>Thanks for subscribing!</h1>
-    <p>A confirmation email has been sent to <strong>${email}</strong>. </p>
-    <p>Please, open it and click the button to confirm your subscription </p>
+    <img class="succes__img" src="../images/icon-success.svg" alt="success icon">
+    <h1 class="succes__title">Thanks for subscribing!</h1>
+    <div class="succes__text">
+        <p>A confirmation email has been sent to <strong>${email}</strong>. </p>
+        <p>Please, open it and click the button to confirm your subscription </p>
+    </div>
 
-    <button class="button-sent">
-        Dismiss message
-    </button>`;
+    <a class="succes__link" href="../../index.html">
+     <button class="succes__button">
+        Dismiss Message
+     </button>
+   </a>`;
     
     // Adicione o elemento criado ao corpo do documento
     document.body.appendChild(criaCard);
 }
+
+const buttonSucced = document.querySelector("succes__button")
+
+
 
 
 
